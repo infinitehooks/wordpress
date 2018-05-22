208521d838e57c46d906c46660cea6c8593c5753
Christian Fraunholz <christian@php10.de>
2018-05-22 08:48:09 +0200

    add full page cache

--- index.php
+++ index.php
@@ -5,13 +5,19 @@
  *
  * @package WordPress
  */
-
+if (file_exists('homepage.cache')) {
+  echo file_get_contents('homepage.cache');
+  exit;
+}
 /**
  * Tells WordPress to load the WordPress theme and output it.
  *
  * @var bool
  */
 define('WP_USE_THEMES', true);
-
+ob_start();
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+$html = ob_get_contents();
+file_put_contents('homepage.cache',$html);
+ob_end_flush();
